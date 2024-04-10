2c4731f8431536ee97ecead4d22d7bd28a3ca99a
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-10 18:09:10 +0200

    add: fuction cronometro

---
+++ cronometro/js/functions.js
@@ -0,0 +1,34 @@
+/**
+ * Description placeholder
+ * @date 10/4/2024 - 16:52:16
+ *
+ * @param {number} second
+ */
+function cronometroFunctions() { // creo funzione cronometro
+    let second = 0;
+    let minutes = 0;
+    const cronometro = setInterval(function () {
+
+        if (second < 60) {
+            second++; // ad ogni interazione aumento i secondi di uno
+        } else {
+            second = 0;  // se ci sono 60 secondi incremento di 1 i minuti e resetto i secondi
+            minutes++
+        }
+        console.log(second, minutes);
+        printHtml(minutes, second); // stampo il risultato
+    }, 1000);
+
+}
+/**
+ * Description placeholder
+ * @date 10/4/2024 - 17:07:25
+ *
+ * @param {*} items
+ */
+function printHtml(minutes, second) {
+    second = second <= 9 ? "0" + second : second; // aggiungo lo zero se il numero è auna cifra
+    minutes = minutes <= 9 ? "0" + minutes : minutes // aggiungo lo zero se il numero è auna cifra
+    document.getElementById("cronometro").innerHTML = minutes + " : " + second;
+
+}
